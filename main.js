6d88dba15b2eb7a8a9a9c862a7c99da30f947c3a
sefadalklc <sefadalk11@gmail.com>
2022-07-23 22:41:14 +0300

    finish project

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ let search = document.querySelector(".search-box");
 
 document.querySelector("#search-icon").onclick = () => {
   search.classList.toggle("active");
+  menu.classList.remove("active");
 };
+
+let menu = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () => {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+// Hide Menu And Search Box On Scroll
+window.onscroll = () => {
+  menu.classList.remove("active");
+  search.classList.remove("active");
+};
+
+//Header
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
